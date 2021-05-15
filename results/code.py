74cd45bf9098c400d2efb310994814a217ac82e0
bormaa <e.emil@nu.edu.eg>
2021-05-15 21:21:11 +0200

    edited code to get data and create objects

--- results/code.py
+++ results/code.py
@@ -1,7 +1,24 @@
+class data:
+  def __init__(self, Temperature, Humidity,Pressure):
+    self.Temperature = Temperature
+    self.Humidity = Humidity
+    self.Pressure = Pressure
 txtdata = open("1.txt", "r")
-# print(txtdata)
+sensorsdic={}
 for line in txtdata:
-    splitarray=line.split("]")
-    print(splitarray)
-    break
-    
+    splitarray=line.split("	")
+    if splitarray[2][0]=="f":
+        splitaddr=splitarray[2].split("[")
+        addr=splitaddr[0].strip()
+        getdata=splitaddr[1].split(":")
+        Temp=getdata[3].split(",")[0].strip()
+        humidity=getdata[4].split(",")[0].strip()
+        Pressure=getdata[5].split("\n")[0].strip()
+        # print(getdata)
+        # print(Pressure)
+        # print(addr)
+        newobj=data(Temp,humidity,Pressure)
+        sensorsdic.setdefault(addr,[]).append(newobj)
+
+    # break
+print(len(sensorsdic["fd00::206:6:6:6"]))
